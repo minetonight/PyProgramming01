3d5c58ebfc19ac41549fb7d30377651914b28624
koko2003 <kokobialkov1@gmail.com>
2014-07-29 17:24:20 +0300

    Update ktb_nagore_na_dolo.py

--- ktb_nagore_na_dolo.py
+++ ktb_nagore_na_dolo.py
@@ -1,2 +1,8 @@
 import random
 
+max = input("kakvo maksimalno chislo iskash: " )
+min = input("kakvo minimalno chislo iskash: " )
+
+while True
+   comytarno_chislo = random.randint(max,min)
+   print
