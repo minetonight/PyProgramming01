dd25f862e6046866d2c3894d37a6f5787d443f02
Ivan-Lupov <krasun80@abv.bg>
2014-07-29 17:52:52 +0300

    Update Up_down Ivan-Lupov.py

--- Up_down Ivan-Lupov.py
+++ Up_down Ivan-Lupov.py
@@ -1,3 +1,15 @@
 import random
 
-for chislo in range(10)
+max = input("What maximum number want: ")
+min = input("What minimum number want: ")
+while True:
+  
+  com_number = random.randint(min,max)
+  print("Up or down  including" + str(com_number))
+  my_number = input("1 for up, 2 for down, 3 for Yes: ")
+  if my_number == 1
+    min = com_number + 1
+  elif my_number == 2
+    max = max - (com_number + 1) 
+  else:
+    break
