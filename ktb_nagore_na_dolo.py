d458d1e7ecc0c0885a9f54748a8f806b89ea4ca6
koko2003 <kokobialkov1@gmail.com>
2014-07-29 17:43:51 +0300

    Update ktb_nagore_na_dolo.py

--- ktb_nagore_na_dolo.py
+++ ktb_nagore_na_dolo.py
@@ -4,5 +4,13 @@ max = input("kakvo maksimalno chislo iskash: " )
 min = input("kakvo minimalno chislo iskash: " )
 
 while True
-   comytarno_chislo = random.randint(max,min)
-   print
+   com_chislo = random.randint(max,min)
+   print("nagore ili nadolo e moeto" str(comchislo))
+   moeto_chislo = input("1 e nagore, 2 e nadolo,3 e vyarno")
+   if moeto_chislo == 1:
+      min = com_chislo + 1
+   elif moeto_chislo == 2:
+      max = max - com_chislo
+   else moeto_chislo == 3:
+      print ("pecelish")
+      break
