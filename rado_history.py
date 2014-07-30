a485e380b832ee9d2eb05f0367065291705dc241
raga70 <raga70@abv.bg>
2014-07-30 17:18:17 +0300

    e
    fds

---
+++ rado_history.py
@@ -0,0 +1,85 @@
+import turtle
+
+t = turtle.Turtle()
+
+
+
+
+for c in ['pink', 'pink', 'pink', 'pink']:
+    t.color(c)
+    t.forward(80)
+    t.left(90)
+    
+    
+    
+    
+for c in ['pink']:
+    t.color(c)
+    t.forward(40)
+    t.left(90)
+
+for c in ['pink']:
+    t.color(c)
+    t.left(90)
+    t.left(90)
+    t.forward(30)
+
+
+   
+for c in ['pink']:
+    t.color(c)
+    t.left(90)
+    t.forward(40)
+    
+    
+    
+
+for c in ['pink']:
+    t.color(c)
+    t.left(90)
+    t.left(90)
+    t.left(90)
+    t.left(90)
+    t.forward(40)
+    
+    
+for c in ['pink']:
+    t.color(c)
+    t.right(90)
+    t.right(90)
+    t.forward(160)
+
+
+for c in ['pink']:
+    t.color(c)
+    t.right(90)
+    t.right(90)
+    t.forward(80)    
+    
+   
+    
+for c in ['pink']:
+    t.color(c)
+    t.right(90)
+    t.forward(80)    
+    
+    
+    
+    
+for c in ['pink']:
+    t.color(c)
+    t.right(45)
+    
+    t.forward(80)    
+    
+    
+for c in ['pink']:
+    t.color(c)
+    t.right(180)
+    t.forward(80)    
+    
+
+for c in ['pink']:
+    t.color(c)
+    t.right(90)
+    t.forward(80)    
