1b941cdc48e129fe4ab8a356f5654470f428dd06
raga70 <raga70@abv.bg>
2014-07-30 18:49:51 +0300

    e
    w

--- rado_history.py
+++ rado_history.py
@@ -83,3 +83,108 @@ for c in ['pink']:
     t.color(c)
     t.right(90)
     t.forward(80)    
+    
+    
+    
+    
+    
+    
+    
+    
+for c in ['white']:
+    t.color(c)
+    t.right(315)
+    t.forward(140) 
+    
+  
+    
+    
+for c in ['green']:
+    t.color(c)
+    t.right(315)
+    t.forward(3) 
+    
+    
+for c in ['green']:
+    t.color(c)
+    t.right(90)
+    t.forward(30) 
+      
+    
+for c in ['green']:
+    t.color(c)
+    t.right(180)
+    t.forward(30) 
+       
+for c in ['green']:
+    t.color(c)
+    t.right(315)
+    t.forward(30) 
+  
+    
+for c in ['green']:
+    t.color(c)
+    t.right(270)
+    t.forward(30) 
+  
+for c in ['green']:
+    t.color(c)
+    t.right(90)
+    t.forward(14) 
+ 
+    
+    
+for c in ['green']:
+    t.color(c)
+    t.right(90)
+    t.forward(30) 
+    
+    
+    
+for c in ['green']:
+    t.color(c)
+    t.right(270)
+    t.forward(30)
+    
+    
+for c in ['green']:
+    t.color(c)
+    t.right(270)
+    t.forward(30)
+    
+
+for c in ['green']:
+    t.color(c)
+    t.right(90)
+    t.forward(15)
+    
+for c in ['green']:
+    t.color(c)
+    t.right(90)
+    t.forward(30)
+ 
+for c in ['green']:
+    t.color(c)
+    t.right(270)
+    t.forward(40)    
+    
+for c in ['green']:
+    t.color(c)
+    t.right(90)
+    t.forward(30)
+    
+for c in ['green']:
+    t.color(c)
+    t.right(90)
+    t.forward(30)
+    
+    
+for c in ['green']:
+    t.color(c)
+    t.right(90)
+    t.forward(13)
+    
+for c in ['green']:
+    t.color(c)
+    t.right(270)
+    t.forward(90)
