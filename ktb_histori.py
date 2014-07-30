56e6eba198eb675e4d7a83d41ed69337defca151
koko2003 <kokobialkov1@gmail.com>
2014-07-30 18:48:05 +0300

    Create ktb_histori.py

---
+++ ktb_histori.py
@@ -0,0 +1,74 @@
+import random
+import time
+
+print("This is the story of a shadow ")
+print(" ")
+time.sleep(3.00)
+print("It was called the Queen of Shadows ")
+print(" ")
+time.sleep(5.00)
+print("your task is to save her from the evil people.")
+print("  ")
+time.sleep(4.00)
+
+
+print("                              Level 1 ")
+print("  ")
+sqnkata = input(" before you have two people in the shadow you have two choices 1 bypass them or 2 kill them ")
+if sqnkata == 1:
+	print("	next level ")
+	sqnkata = 0
+if sqnkata == 2:
+	print("								Level 1 ")
+	print("  ")
+	sqnkata = input(" before you have two people in the shadow you have two choices 1 bypass them or 2 kill them ")
+
+
+
+
+print("                                 Level 2 ")
+print("  ")
+sqnkata = input(" before you now a man with a lantern who can not see and 4 people shadow1 goes to the man with the lantern 4 Dead 2 shadowem ")
+if sqnkata == 1:
+	print("                                 Level 2 ")
+	print("  ")
+	sqnkata = input(" before you now a man with a lantern who can not see and 4 people shadow1 goes to the man with the lantern 4 Dead 2 shadowem ")
+if sqnkata == 2:
+	print("	next level ")
+	sqnkata = 0
+		
+		
+		
+print("                                 Level 3 ")
+sqnkata = input(" you're the prison cell with the Queen before you have 2 many armed guards 1 create shade and kill them, 2 rolls to save her. ")
+if sqnkata == 1:
+	print("next level ")
+	sqnkata = 0
+if sqnkata == 2:
+	print("                                 Level 3 ")
+	sqnkata = input(" you're the prison cell with the Queen before you have 2 many armed guards 1 create shade and kill them, 2 rolls to save her. ")
+	sqnkata = 0
+	
+	
+print("                                 Final ")
+print("reached the final hurray!!! : P: P: P: P: P: P: P: P: P: P: P: P: P: P you win you win you win   ")
+
+
+print("  ")
+print("  ")
+print("  ")
+print("  ")
+print("  ")
+print("  ")
+print("  ")
+print("  ")
+print("  ")
+print("  ")
+print("  ")
+print("  ")
+print("  ")
+print("  ")
+print("  ")
+print("a production robopartans")
+print("producer and screenwriter: Kaloyan Bialkov")
+
